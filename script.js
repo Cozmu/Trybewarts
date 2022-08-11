7201654eeebe2ca09cb22f031439642d5c656505
Cozmu <www.jorgereis@gmail.com>
2022-08-11 18:03:59 -0300

    Adicionando requisito 20

--- script.js
+++ script.js
@@ -3,25 +3,33 @@ let senha = document.getElementById('password');
 
 function validacao() {
     if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-       window.alert('Olá, Tryber!')
+        window.alert('Olá, Tryber!')
     } else {
         window.alert('Email ou senha inválidos.')
     }
 }
 
-// Codigo reutilizado desse site https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
-// Função que habilita o botão Enviar.
 let botaoLoguin = document.getElementById('botao-loguin');
 botaoLoguin.addEventListener('click', validacao);
 
+// Codigo reutilizado desse site https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
+// Função que habilita o botão Enviar.
 document.getElementById('submit-btn').disabled = true;
 
-document.getElementById('agreement').addEventListener('input', function(event){
+document.getElementById('agreement').addEventListener('input', function (event) {
     let conteudo = document.getElementById('agreement').value;
 
-    if (conteudo !== null && conteudo !== ''){
+    if (conteudo !== null && conteudo !== '') {
         document.getElementById('submit-btn').disabled = false;
     } else {
         document.getElementById('submit-btn').disabled = true;
     }
 })
+
+// Função que conta as caracteres do campo de texto
+document.getElementById('textarea').addEventListener('input', function () {
+    let caracteres = this.value.length;
+    let contador = document.getElementById('counter');
+    contador.innerText = 500 - caracteres;
+})
+
