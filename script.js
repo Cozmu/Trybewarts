69381bf67bb00f75c45aa10a7abfa117e908e645
Cozmu <www.jorgereis@gmail.com>
2022-08-11 20:28:35 -0300

    Finalizado requisito 20

--- script.js
+++ script.js
@@ -33,3 +33,85 @@ document.getElementById('textarea').addEventListener('input', function () {
     contador.innerText = 500 - caracteres;
 })
 
+let divPai = document.getElementById('teste');
+let formPreenchido = document.getElementById('formulario-preenchido');
+
+let submitBotao = document.getElementById('submit-btn');
+let nome = document.getElementById('input-name');
+let sobrenome = document.getElementById('input-lastname');
+let getEmail = document.getElementById('input-email');
+let select = document.getElementById('house');
+let classFamily = document.querySelectorAll('.class-family');
+let checkBox = document.querySelectorAll('.subject');
+let classAvaliacao = document.querySelectorAll('.class-avaliacao');
+let observacao = document.getElementById('textarea');
+
+
+function getHouse() {
+    return select.options[select.selectedIndex].value;
+}
+function getRadius() {
+    for (let index = 0; index < classFamily.length; index += 1) {
+        if (classFamily[index].checked) {
+            return classFamily[index].value;
+        }
+    }
+}
+function getCheck() {
+    let textA = [];
+    for (let index = 0; index < checkBox.length; index += 1) {
+        if (checkBox[index].checked) {
+            textA.push(checkBox[index].value)
+        }
+    }
+    return textA.join(', ');
+}
+function getAvaliacao() {
+    for (let index = 0; index < classAvaliacao.length; index += 1) {
+        if (classAvaliacao[index].checked) {
+            return classAvaliacao[index].value;
+        }
+    }
+}
+
+function lista() {
+    let nomeCompleto = document.createElement('p');
+    let email = document.createElement('p');
+    let houseOptions = document.createElement('p');
+    let familia = document.createElement('p');
+    let materias = document.createElement('p');
+    let avaliacao = document.createElement('p');
+    let obs = document.createElement('p');
+
+    nomeCompleto.innerText = 'Nome: ' + nome.value + sobrenome.value;
+    email.innerText = 'Email: ' + getEmail.value;
+    houseOptions.innerText = 'Casa: ' + getHouse();
+    familia.innerText = 'Familia: ' + getRadius();
+    materias.innerText = 'Matérias: ' + getCheck();
+    avaliacao.innerText = 'Avaliação: ' + getAvaliacao();
+    obs.innerText = 'Observações: ' + observacao.value;
+
+    divPai.appendChild(nomeCompleto);
+    divPai.appendChild(email);
+    divPai.appendChild(houseOptions);
+    divPai.appendChild(familia);
+    divPai.appendChild(materias);
+    divPai.appendChild(avaliacao);
+    divPai.appendChild(obs);
+
+    saveLocalStorage();
+}
+submitBotao.addEventListener('click', lista);
+
+function saveLocalStorage() {
+    localStorage.setItem('inscricao', divPai.innerHTML);
+}
+function loadLocalStorage() {
+    const load = localStorage.getItem('inscricao');
+    if (load) {
+        divPai.innerHTML = load;
+    }
+}
+
+window.onload = loadLocalStorage;
+
