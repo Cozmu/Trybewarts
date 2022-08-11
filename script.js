8a396f4250d4b6e9825a72e4f82655ee44f1102f
Bruno <brunobrito728@gmail.com>
2022-08-11 16:12:13 -0300

    Finalizado requisito 17

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let email = document.getElementById('email');
 let senha = document.getElementById('password');
-console.log(email);
+const checkin = document.getElementById('agreement');
+const buttonEnviar = document.getElementById('submit-btn');
 
-function validacao(event) {
+function validacao() {
     if (email.value === 'tryber@teste.com' && senha.value === '123456') {
        window.alert('Olá, Tryber!')
     } else {
@@ -12,3 +13,13 @@ function validacao(event) {
 
 let botaoLoguin = document.getElementById('botao-loguin');
 botaoLoguin.addEventListener('click', validacao);
+
+// function enviar (){
+//     if (checkin === true){
+//         console.log('Chegou aqui!!')
+//         buttonEnviar.disabled = false;
+
+//     }
+// }
+
+// checkin.addEventListener('click', enviar);
